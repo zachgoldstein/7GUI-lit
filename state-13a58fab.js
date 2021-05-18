e8534cf9de67b1bcd07ccf1bd0ea4812296dd227
Zachary Goldstein <zachgold@gmail.com>
2021-05-18 09:12:15 -0400

    Initial Commit

---
+++ state-13a58fab.js
@@ -0,0 +1,6 @@
+import{e as t}from"./property-87bb8bd7.js";
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */function r(r){return t({...r,state:!0,attribute:!1})}export{r};
